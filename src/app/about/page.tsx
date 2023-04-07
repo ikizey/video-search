72650448eac9821fc1affe91b1412026a8439052
kizey <ikizey@icloud.com>
2023-04-07 22:23:38 +0400

    change about text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,35 +1,33 @@
 export default function About() {
   return (
     <div className='max-w-6xl mx-auto space-y-4 p-4'>
-      <h1 className='text-2xl font-medium text-blue-400'>About</h1>
+      <h1 className='text-2xl font-medium text-blue-400'>About Us</h1>
       <p>
         Welcome to our movie database website! We are a team of passionate movie
-        enthusiasts who have come together to create a one-stop destination for
-        all your movie-related needs.
+        enthusiasts who have come together to create a comprehensive destination
+        for all things cinema.
       </p>
-
       <p>
-        Our website is designed to provide you with a comprehensive database of
-        movies from all around the world, along with the latest news, reviews,
-        and trailers. Our movie database is constantly updated with new
-        releases, ensuring that you have access to the latest and greatest in
-        the world of cinema. You can search for movies by title, director,
-        actor, genre, or release date, making it easy to find the perfect movie
-        for any occasion.
+        Our website offers an extensive movie database featuring films from
+        around the world, as well as the latest news, reviews, and trailers. Our
+        movie database is regularly updated with new releases, ensuring that you
+        always have access to the latest and greatest in the world of cinema.
+        You can search for movies by title, director, actor, genre, or release
+        date, making it easy to find the perfect movie for any occasion.
       </p>
-
       <p>
-        In addition to our extensive movie database, we also offer a platform
-        for movie lovers to connect and share their thoughts on the latest
-        releases. Our community section includes a forum where you can discuss
-        your favorite films with like-minded individuals and read reviews and
-        ratings from other users. We also have a section dedicated to movie news
-        and trailers, keeping you updated with the latest happenings in the
-        world of cinema. Thank you for visiting our website and we hope you
-        enjoy your time browsing through our movie database. If you have any
-        feedback or suggestions, please feel free to contact us. We are always
-        looking for ways to improve and enhance the user experience on our
-        website. Happy browsing!
+        In addition to our movie database, we provide a platform for movie
+        lovers to connect and share their thoughts on the latest releases. Our
+        community section includes a forum where you can discuss your favorite
+        films with like-minded individuals, read reviews and ratings from other
+        users, and stay up-to-date with the latest happenings in the world of
+        cinema.
+      </p>
+      <p>
+        Thank you for visiting our website, and we hope you enjoy browsing our
+        movie database. If you have any feedback or suggestions, please do not
+        hesitate to contact us. We are always looking for ways to improve and
+        enhance the user experience on our website. Happy browsing!
       </p>
     </div>
   );
