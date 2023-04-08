15eda43028a981ddb16a3b59a923d5e72c961c9d
kizey <ikizey@icloud.com>
2023-04-08 04:31:58 +0400

    remove p about forum

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,16 +12,7 @@ export default function About() {
         around the world, as well as the latest news, reviews, and trailers. Our
         movie database is regularly updated with new releases, ensuring that you
         always have access to the latest and greatest in the world of cinema.
-        You can search for movies by title, director, actor, genre, or release
-        date, making it easy to find the perfect movie for any occasion.
-      </p>
-      <p>
-        In addition to our movie database, we provide a platform for movie
-        lovers to connect and share their thoughts on the latest releases. Our
-        community section includes a forum where you can discuss your favorite
-        films with like-minded individuals, read reviews and ratings from other
-        users, and stay up-to-date with the latest happenings in the world of
-        cinema.
+        You can search for movies by it&apos;s title.
       </p>
       <p>
         Thank you for visiting our website, and we hope you enjoy browsing our
